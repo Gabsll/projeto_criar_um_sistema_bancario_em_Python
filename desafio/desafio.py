778e3c8661df93db0716a30a544b8ba839134487
Gabsll <gabrielsr0302@gmail.com>
2023-07-20 18:13:37 -0300

    v1

--- desafio/desafio.py
+++ desafio/desafio.py
@@ -3,6 +3,7 @@ menu = '''
 [D] - Depositar
 [S] - Sacar
 [E] - Extrato
+[N] - Operações realizadas
 [F] - Sair
 ==========================
 ==> '''
@@ -30,14 +31,14 @@ while True:
             print(f"Operação falhou! O valor do deposito excede o limite: R${limite_deposito_dia}")
         elif excedeu_numero_deposito:
             print("Operação falhou! Número máximo de depositos excedido.")
-            print(f"Numero de operaçoes realizada: {numero_de_depositos}")
+            print(f"Numero de operações realizada: {numero_de_depositos}")
             print(F"Limite de Depositos: {LIMITE_DE_DEPOSITOS}")
         elif valor > 0:
             saldo += valor
             numero_de_depositos += 1
             extrato += f"Deposito: R$ {valor:.2f}\n"
             print(f"Valor depositado com sucesso: R${valor: .2f}")
-            print(f"Numero de operaçoes realizada: {numero_de_depositos}")
+            print(f"Numero de operações realizada: {numero_de_depositos}")
             
         else:
             print("valor invalido, insira um valor valido.")
@@ -61,7 +62,7 @@ while True:
             print(f"Operação falhou! O valor do saque excede o limite: R${limite_saque_dia}")
         elif excedeu_saques:
             print("Operação falhou! Número máximo de saques excedido.")
-            print(f"Numero de operaçoes realizada: {numero_de_saques}")
+            print(f"Numero de operações realizada: {numero_de_saques}")
             print(F"Limite de saques: {LIMITE_DE_SAQUES}")
 
         elif valor > 0:
@@ -69,7 +70,7 @@ while True:
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_de_saques += 1
             print(f"Valor sacado com sucesso: R${valor: .2f}")
-            print(f"Numero de operaçoes realizada: {numero_de_saques}")
+            print(f"Numero de operações realizada: {numero_de_saques}")
         else:
             print("Operação falhou! O valor informado é inválido.")
 
@@ -77,8 +78,17 @@ while True:
         print()
         print(" Extrato ".center(36, "="))
         print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
         print("".center(36, "="))
+        print(f"Saldo: R$ {saldo:.2f}")
+        print("".center(36, "="))
+        
+    elif opcao == "n"or opcao == "N":
+        print(" Operações realizadas ".center(36, "="))
+        print(f"Operações de saque realizadas: {numero_de_saques}")
+        print(f"Limite de saques diario: {LIMITE_DE_SAQUES}")
+        print(f"Operações de deposito realizadas: {numero_de_depositos}")
+        print(f"Limite de depositos diario: {LIMITE_DE_DEPOSITOS}")
+        print("".center(36, "="))        
     elif opcao == "f" or opcao == "F":
         break
 
